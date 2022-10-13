a6e090fd7fb46a7164c147fe72e7f30ed85e8fe9
nickbrollop <nbpolita@ucs.br>
2022-10-13 16:17:36 -0300

    commit 3 13-10

--- enderecos/views.py
+++ enderecos/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from myapp.forms import ContactForm
+from django.views.generic.edit import FormView
 # Create your views here.
+
+class ContactFormview(FormView):
+    template_name ='contato.html'
+    form_class = ContactForm
+    success_url ="/Obrigada/"
