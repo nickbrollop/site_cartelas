b5a4b169001b10a0bae5fbb08a17e565b344b5e9
nickbrollop <nbpolita@ucs.br>
2022-10-15 08:51:39 -0300

    commit 5 15-10

--- enderecos/admin.py
+++ enderecos/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-
+from models import cidades, loc_Entrega
 # Register your models here.
 
+admin.site.register(cidades, loc_Entrega)
--- materiais/admin.py
+++ materiais/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from models import material, tipo_Material
 
 # Register your models here.
+
+admin.site.register(material, tipo_Material)
