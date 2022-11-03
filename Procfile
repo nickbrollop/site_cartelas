ab4fef5e57f625809eedce96e1ada92fc29a48ef
nickbrollop <nbpolita@ucs.br>
2022-11-03 16:03:43 -0300

    03-11 commit 1

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn domi_laura.wsgi
--- domi_laura/settings.py
+++ domi_laura/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+# Configure Django App for Heroku.
+import django_heroku
+
+django_heroku.settings(locals())
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -135,3 +140,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
